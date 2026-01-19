19ad3e6955854d6a294792613933a442a8c4de42
Chetan Patil <patilvchetan@gmail.com>
2026-01-19 11:01:55 +0530

    Implement simple interest calculation in si.py

---
+++ si.py
@@ -0,0 +1,5 @@
+p=int(input("enter the principal amount: "))
+r=float(input("enter the rate of interest: "))
+t=int(input("enter the time in years: "))
+si=(p*r*t)/100
+print(f"the simple interest is: {si}")
